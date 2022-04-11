a137e35e293414c1e55ee9c29b5b1b36e8901ce6
26164003 <26164003@L815WD220009.edu.cbe.ab.ca>
2022-04-11 13:23:58 -0600

    1:23PM

--- Chapt3/src/Exercises/Ex1.java
+++ Chapt3/src/Exercises/Ex1.java
@@ -1,10 +1,51 @@
 package Exercises;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{//
+		System.out.println("|\\      |");
+	    System.out.println("| \\     |");
+		System.out.println("|  \\    |");
+		System.out.println("|   \\   |");
+		System.out.println("|    \\  |");
+		System.out.println("|     \\ |");
+		System.out.println("|      \\|");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("__________");
+		System.out.println("    |     ");
+		System.out.println("    |     ");
+		System.out.println("    |     ");
+		System.out.println("    |     ");
+		System.out.println("    |     ");
+		System.out.println("____|_____");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("__________");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|_________");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("__________");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|_________");
+		System.out.println("         |");
+		System.out.println("         |"); 
+		System.out.println("         |");
+		System.out.println("_________|");
+		
 	}
-
+	
+	
 }
